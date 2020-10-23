c9497bd0ce34782b1c722670e52cc523b26d2983
Ricardo Fernandez <ricardojafernandez@hotmail.com>
2020-10-23 13:44:41 +0100

    removed v2 files

--- ZermiaV2_independent/Zermia/src/main/java/zermia/faults/ThreadDelayFault.java
+++
@@ -1,9 +0,0 @@
-package zermia.faults;
-
-public class ThreadDelayFault {
-	protected Integer faultDuration;
-	
-	public void executeFault(Integer duration) throws InterruptedException {
-		Thread.sleep(duration);
-	}	
-}
