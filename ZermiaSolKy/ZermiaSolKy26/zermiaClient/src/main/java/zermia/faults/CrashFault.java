ca0368181b35b3ec848a821884c665a1aac5c6f8
Ricardo Fernandez <ricardojafernandez@hotmail.com>
2021-06-29 20:55:36 +0100

    removed v5 files

--- ZermiaSolKy/ZermiaSolKy26/zermiaClient/src/main/java/zermia/faults/CrashFault.java
+++
@@ -1,9 +0,0 @@
-package zermia.faults;
-
-public class CrashFault {
-
-	public void executeFault() {
-		System.out.println("Crash");
-		System.exit(-1);
-		}	
-}
--- ZermiaSolKy/zermiaClient/src/main/java/zermia/faults/ThreadDelayFault.java
+++
@@ -1,9 +0,0 @@
-package zermia.faults;
-
-public class ThreadDelayFault{
-
-	public void executeFault(Integer duration) throws InterruptedException {
-		Thread.sleep(duration);
-	}
-
-}
